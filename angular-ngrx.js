119dfe0b3bbdb862302716d74d1062bf2106482f
Edouard <ed@getlarge.eu>
2024-10-09 07:49:25 +0000

    fix: apply requested changes

--- angular-ngrx.js
+++ angular-ngrx.js
@@ -11,22 +11,22 @@ module.exports = {
 
       rules: {
         // CUSTOMIZED RULES FROM EXTENDED CONFIGS
+
+        // ADDITIONAL RULES
+
+        // https://github.com/ngrx/platform/tree/main/modules/eslint-plugin
         '@ngrx/avoid-combining-component-store-selectors': 'warn',
         '@ngrx/avoid-cyclic-effects': 'error',
         '@ngrx/avoid-duplicate-actions-in-reducer': 'error',
         '@ngrx/avoid-mapping-component-store-selectors': 'warn',
         '@ngrx/updater-explicit-return-type': 'warn',
-        '@ngrx/avoid-cyclic-effects': 'warn',
         '@ngrx/no-dispatch-in-effects': 'warn',
         '@ngrx/no-effects-in-providers': 'error',
         '@ngrx/no-multiple-actions-in-effects': 'off',
-        '@ngrx/prefer-action-creator-in-of-type': 'warn',
-        '@ngrx/prefer-concat-latest-from': 'warn',
         '@ngrx/prefer-effect-callback-in-block-statement': 'off',
         '@ngrx/use-effects-lifecycle-interface': 'warn',
         '@ngrx/avoid-combining-selectors': 'warn',
         '@ngrx/avoid-dispatching-multiple-actions-sequentially': 'warn',
-        '@ngrx/avoid-duplicate-actions-in-reducer': 'warn',
         '@ngrx/avoid-mapping-selectors': 'warn',
         '@ngrx/good-action-hygiene': 'warn',
         '@ngrx/no-multiple-global-stores': 'warn',
@@ -42,8 +42,6 @@ module.exports = {
         '@ngrx/prefix-selectors-with-select': 'warn',
         '@ngrx/select-style': 'warn',
         '@ngrx/use-consistent-global-store-name': ['warn', 'store$'],
-
-        // ADDITIONAL RULES
 
         // https://github.com/cartant/eslint-plugin-rxjs#rules
         'rxjs/no-unsafe-catch': 'error',

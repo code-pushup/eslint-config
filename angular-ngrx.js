e1105887982bc53f4887b743b5a16367c67ddfd3
Edouard Maleix <ed@getlarge.eu>
2024-10-09 13:53:28 +0200

    feat: add support for NgRx 18 (#23)
    * feat: add support for NgRx 18
    
    * fix: apply requested changes
    
    * refactor: revert rules removal

--- angular-ngrx.js
+++ angular-ngrx.js
@@ -6,21 +6,42 @@ module.exports = {
     {
       files: TYPESCRIPT_FILE_PATTERNS,
 
-      extends: [
-        '@code-pushup/eslint-config/angular',
-        'plugin:@ngrx/recommended-requiring-type-checking',
-      ],
+      plugins: ['@ngrx'],
+      extends: ['@code-pushup/eslint-config/angular'],
 
       rules: {
         // CUSTOMIZED RULES FROM EXTENDED CONFIGS
 
+        // ADDITIONAL RULES
+
+        // https://github.com/ngrx/platform/tree/main/modules/eslint-plugin
+        '@ngrx/avoid-combining-component-store-selectors': 'warn',
         '@ngrx/avoid-cyclic-effects': 'error',
         '@ngrx/avoid-duplicate-actions-in-reducer': 'error',
-        '@ngrx/no-multiple-actions-in-effects': 'off',
-        '@ngrx/prefer-effect-callback-in-block-statement': 'off',
+        '@ngrx/avoid-mapping-component-store-selectors': 'warn',
+        '@ngrx/updater-explicit-return-type': 'warn',
+        '@ngrx/no-dispatch-in-effects': 'warn',
+        '@ngrx/no-effects-in-providers': 'error',
+        '@ngrx/prefer-action-creator-in-of-type': 'warn',
+        '@ngrx/prefer-concat-latest-from': 'warn',
+        '@ngrx/use-effects-lifecycle-interface': 'warn',
+        '@ngrx/avoid-combining-selectors': 'warn',
+        '@ngrx/avoid-dispatching-multiple-actions-sequentially': 'warn',
+        '@ngrx/avoid-mapping-selectors': 'warn',
+        '@ngrx/good-action-hygiene': 'warn',
+        '@ngrx/no-multiple-global-stores': 'warn',
+        '@ngrx/no-reducer-in-key-names': 'warn',
+        '@ngrx/no-store-subscription': 'warn',
+        '@ngrx/no-typed-global-store': 'warn',
+        '@ngrx/on-function-explicit-return-type': 'warn',
+        '@ngrx/prefer-action-creator-in-dispatch': 'warn',
+        '@ngrx/prefer-action-creator': 'warn',
+        '@ngrx/prefer-inline-action-props': 'warn',
+        '@ngrx/prefer-one-generic-in-create-for-feature-selector': 'warn',
+        '@ngrx/prefer-selector-in-select': 'warn',
+        '@ngrx/prefix-selectors-with-select': 'warn',
+        '@ngrx/select-style': 'warn',
         '@ngrx/use-consistent-global-store-name': ['warn', 'store$'],
-
-        // ADDITIONAL RULES
 
         // https://github.com/cartant/eslint-plugin-rxjs#rules
         'rxjs/no-unsafe-catch': 'error',

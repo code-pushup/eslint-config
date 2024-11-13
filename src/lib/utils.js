5ec78cf2592d6ade219cd22094f69af0bf379423
Matěj Chalk <matej.chalk@flowup.cz>
2024-11-13 13:21:43 +0100

    Remove prettier config (not needed)

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,20 +1,6 @@
 // @ts-check
 
 /**
- * Checks if package is available
- * @param {string} name name of the package
- * @returns {boolean}
- */
-export function packageExists(name) {
-  try {
-    require.resolve(name);
-    return true;
-  } catch {
-    return false;
-  }
-}
-
-/**
  * Changes all errors to warnings in rule config
  * @param {import('eslint').Linter.RulesRecord} rules rules config (e.g. recommended)
  * @returns {import('eslint').Linter.RulesRecord}

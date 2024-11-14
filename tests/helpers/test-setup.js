7a54b09a2b37809db464c8ca6043b0fb3fd4360f
Matěj Chalk <matej.chalk@flowup.cz>
2024-11-14 18:08:36 +0100

    Remove --legacy-peer-deps flag from test setup

--- tests/helpers/test-setup.js
+++ tests/helpers/test-setup.js
@@ -2,9 +2,9 @@ import { execSync } from 'node:child_process';
 
 export function setup() {
   execSync('npm link');
-  execSync('npm link @code-pushup/eslint-config --legacy-peer-deps');
+  execSync('npm link @code-pushup/eslint-config');
 }
 
 export function teardown() {
-  execSync('npm unlink @code-pushup/eslint-config --legacy-peer-deps');
+  execSync('npm unlink @code-pushup/eslint-config');
 }

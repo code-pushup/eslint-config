57f61b8c697c93231c3ab52b96b0cf2fcbdf091e
Matěj Chalk <matej.chalk@flowup.cz>
2024-11-14 17:50:54 +0100

    Configure self-linting appropriately

--- eslint.config.js
+++ eslint.config.js
@@ -3,4 +3,15 @@
 import tseslint from 'typescript-eslint';
 import cpeslint from './src/index.js';
 
-export default tseslint.config(...cpeslint.javascript, ...cpeslint.vitest);
+export default tseslint.config(...cpeslint.node, ...cpeslint.vitest, {
+  rules: {
+    '@typescript-eslint/no-magic-numbers': 'off',
+    'import/extensions': ['error', { js: 'always', json: 'never' }],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: true,
+      node: true,
+    },
+  },
+});

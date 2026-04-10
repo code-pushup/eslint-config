de6038851e793b84963e42a139031ac2afeba91b
hanna-skryl <hanna.skryl@gmail.com>
2026-04-10 15:19:34 -0400

    fix(eslint-config): scope dependency-checks rule

--- packages/eslint-config/eslint.config.js
+++ packages/eslint-config/eslint.config.js
@@ -1,8 +1,30 @@
 // @ts-check
 
 import { defineConfig } from 'eslint/config';
+import * as jsoncParser from 'jsonc-eslint-parser';
 import baseConfig from '../../eslint.config.js';
 
-export default defineConfig(...baseConfig, {
-  rules: { '@typescript-eslint/no-magic-numbers': 'off' },
-});
+export default defineConfig(
+  ...baseConfig,
+  { rules: { '@typescript-eslint/no-magic-numbers': 'off' } },
+  {
+    // This package publishes from source, so typecheck stands in for the build target.
+    files: ['**/package.json'],
+    languageOptions: { parser: jsoncParser },
+    rules: {
+      '@nx/dependency-checks': [
+        'error',
+        {
+          buildTargets: ['typecheck'],
+          ignoredFiles: [
+            '{projectRoot}/scripts/**',
+            '{projectRoot}/tests/**',
+            '{projectRoot}/vitest.config.js',
+            '{projectRoot}/eslint.config.js',
+          ],
+          ignoredDependencies: ['eslint-import-resolver-typescript'],
+        },
+      ],
+    },
+  },
+);
